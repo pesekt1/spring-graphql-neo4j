369acc2208b1291e3f47a35ef49267dcb13317ef
Tomas Pesek <pesekt@gmail.com>
2022-11-07 14:15:49 +0100

    added queries

--- src/main/java/com/springneo4jgraphql/movie/PersonRepository.java
+++ src/main/java/com/springneo4jgraphql/movie/PersonRepository.java
@@ -1,9 +1,34 @@
 package com.springneo4jgraphql.movie;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.graphql.data.GraphQlRepository;
+
+import java.util.List;
 
 @GraphQlRepository
 public interface PersonRepository extends Neo4jRepository<Person, String>{
     Long deleteByName(String name);
+
+    @Query("""
+            MATCH (m:Movie {title: $title})
+            MATCH (p:Person)
+            Match (p)-[r:ACTED_IN]->(m)
+            return p,r.roles""")
+    List<Object> actorByMovie(@Param("title") String title);
+
+    @Query("""
+            MATCH (m:Movie {title: $title})
+            MATCH (p:Person)
+            Match (p)-[r:DIRECTED]->(m)
+            return p""")
+    List<Person> directorByMovie(@Param("title") String title);
+
+    @Query("""
+            MATCH (m:Movie {title: $title})
+            MATCH (p:Person {name: $name})
+            Match (p)-[r:ACTED_IN]->(m)
+            return r.roles""")
+    Roles getRoles(@Param("title") String title, @Param("name") String name);
 }
