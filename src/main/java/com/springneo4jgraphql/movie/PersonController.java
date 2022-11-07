9a43f7c6ca571a4a833d83615419e43106fb677b
Tomas Pesek <pesekt@gmail.com>
2022-11-07 10:24:52 +0100

    removed List

--- src/main/java/com/springneo4jgraphql/movie/PersonController.java
+++ src/main/java/com/springneo4jgraphql/movie/PersonController.java
@@ -4,8 +4,6 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
-import java.util.List;
-
 @Controller
 public class PersonController {
     private final PersonService personService;
